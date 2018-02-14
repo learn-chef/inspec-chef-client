0fcc048fcff0784f955e09222c6942253174ccea
Thomas Petchel <tpetchel@gmail.com>
2018-02-14 09:49:43 -0500

    Remove conditional logic

--- controls/chef-client.rb
+++ controls/chef-client.rb
@@ -1,28 +1,15 @@
 # encoding: utf-8
-# copyright: 2017, The Authors
+# copyright: 2018, The Authors
 
 title 'chef-client version'
 
-if os[:family] == 'linux' || 'bsd'
-  control 'chef-client-1.0' do
-    impact 1.0
-    title 'Verify the version of chef-client'
-    desc  '
-      This is a profile verifies you are running a modern version of chef client.
-    '
-    describe command('chef-client --version') do
-      its('stdout') { should match (/Chef: 1[3-9]/) }
-    end
-  end
-elsif os[:family] == 'windows'
-  control 'chef-client-1.0' do
-    impact 1.0
-    title 'Verify the version of chef-client'
-    desc  '
-      This is a profile verifies you are running a modern version of chef client.
-    '
-    describe command('chef-client --version') do
-      its('stdout') { should match (/Chef: 1[3-9]/) }
-    end
+control 'chef-client-1.0' do
+  impact 1.0
+  title 'Verify the version of chef-client'
+  desc  '
+    This is a profile verifies you are running a modern version of chef client.
+  '
+  describe command('chef-client --version') do
+    its('stdout') { should match (/Chef: 1[3-9]/) }
   end
 end
